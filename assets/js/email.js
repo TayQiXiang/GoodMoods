9aa2f8b48937183bfd2e219f3b0bdd152e7b161f
Tay Qi Xiang <tayqixiang@gmail.com>
2021-11-19 23:46:35 +0800

    Updated webpage

---
+++ assets/js/email.js
@@ -0,0 +1,38 @@
+function sendEmail(email) {
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "goodmoodswix2002@gmail.com",
+    Password: "BP@sq4Jt2K%WbFk&",
+    To: email,
+    From: "goodmoodswix2002@gmail.com",
+    Subject: "Welcome to GoodMoods",
+    Body: "Hi!!",
+    // Attachments: [
+    //   {
+    //     name: "File_Name_with_Extension",
+    //     path: "Full Path of the file"
+    //   }]
+  }).then(function (message) {
+    alert("Mail has been sent successfully");
+  });
+}
+
+function getInputValue() {
+    // Selecting the input element and get its value
+    var inputVal = document.getElementById("myInput").value;
+
+    // Displaying the value
+    //alert(validateEmail(inputVal));
+
+    if(validateEmail(inputVal)) {
+        sendEmail(inputVal);
+    }
+    else {
+        alert("Invalid Email Address");
+    }
+}
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
---
+++ readm.md
@@ -0,0 +1,15 @@
+#WIX 2002 Project Management
+This is a group project for the WIX 2002 course.
+Group Members:
+1. Frankie Lim Qi Quan
+2. Tay Qi Xiang
+3. Goh Chee Lam
+4. Chai Yu Xuan
+5. Tiow Kit Keong
+
+Credits:
+
+Template Name: Moderna
+Template URL: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/
+Author: BootstrapMade.com
+License: https://bootstrapmade.com/license/
